1d38781fcddc2d4db0161b76abf5bc06f5df6002
Mikhail Epatko <m.epatko@jsa-group.ru>
2025-04-04 09:36:54 +0300

    #2 Add divide func

---
+++ task.go
@@ -0,0 +1,8 @@
+package task
+
+// Напишите функцию, которая будет возвращать результат деления двух чисел
+// Работа функции не должна завершаться паникой
+func Divide(divisible, divider int) (int, error) {
+	//return 0, nil
+	return divisible / divider, nil
+}
